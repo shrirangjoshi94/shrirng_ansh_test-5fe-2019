d29319594193a75c3681411120b3e37947a177c1
Shrirang <shrirangjoshi94@gmail.com>
2019-02-05 13:34:32 +0530

    task 3 logic incomplete

--- calculator.php
+++ calculator.php
@@ -4,34 +4,56 @@
 //print_r($argv[2]);
 //exit;
 
-$sum = 0;
-$count = count($argv);
-if($count > 1)
+try{
+    $sum = 0;
+    $count = count($argv);
+    if($count > 1)
+    {
+    if($argv[1] == "sum" || $argv[1] == "add")
+    {
+    if($count > 2)
+    {
+    if(isset($argv[2]) && is_numeric($argv[2]))
+    {
+        echo $argv[2];
+    }else{
+
+//print_r($argv[2]);exit;
+
+/*
+if($argv[1] == "add") //task 3
 {
-if($argv[1] == "sum" || $argv[1] == "add")
-{
-if($count > 2)
-{
-if(isset($argv[2]) && is_numeric($argv[2]))
-{
-    echo $argv[2];
-}else{
-$temp = explode(",",$argv[2]);
-for($i = 0;$i < count($temp);$i++)
-{
-$sum = $sum + $temp[$i];
+    for($i = 0;$i < $count;$i++)
+    {
+        if(!is_numeric($argv[2][$i])){
+ $temp = explode("n",$arg[2][$i]);
+ $argv[2][$i] = $temp[0];
+ $argv[2][$i] = $temp[1];
+$i++;
+        }
+    }
 }
-echo $sum;
+*/
+    $temp = explode(",",$argv[2]);
+    for($i = 0;$i < count($temp);$i++)
+    {
+    $sum = $sum + $temp[$i];
+    }
+    echo $sum;
+    }
+    }else{
+        echo 0;
+    }
+    }else{
+        echo "math operation name does not match";
+    }
+    }else{
+        echo "Please enter the a math operation name";
+        //return;
+    }
+    echo nl2br("\n");
+}catch(Exception $e){
+    echo 'Message: ' .$e->getMessage();
 }
-}else{
-    echo 0;
-}
-}else{
-    echo "math operation name does not match";
-}
-}else{
-    echo "Please enter the a math operation name";
-    //return;
-}
-echo nl2br("\n");
+
 ?>
