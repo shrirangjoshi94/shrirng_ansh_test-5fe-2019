f773405f726d347bcd8d69f73631e7500ebaf8c2
Shrirang <shrirangjoshi94@gmail.com>
2019-02-05 13:48:47 +0530

    task 5 completed

--- calculator.php
+++ calculator.php
@@ -1,7 +1,7 @@
 <?php
 
 //echo nl2br("\n");
-//print_r($argv[2]);
+//print_r($input);
 //exit;
 
 try{
@@ -13,47 +13,66 @@ try{
     {
     if($count > 2)
     {
-    if(isset($argv[2]) && is_numeric($argv[2]))
+        $input = $argv[2];
+        $temp = explode(",",$input);
+        for($i = 0; $i < count($temp);$i++) //task 5
+        {
+if(is_numeric($temp[$i]))
+{
+if($temp[$i] < 0)
+{
+    echo "Negative numbers not allowed";
+    exit;
+}
+}
+        }
+
+    if(isset($input) && is_numeric($input))
     {
-        echo $argv[2];
+        echo $input;
+        exit;
     }else{
 
-//print_r($argv[2]);exit;
+//print_r($input);exit;
 
 /*
 if($argv[1] == "add") //task 3
 {
     for($i = 0;$i < $count;$i++)
     {
-        if(!is_numeric($argv[2][$i])){
+        if(!is_numeric($input[$i])){
  $temp = explode("n",$arg[2][$i]);
- $argv[2][$i] = $temp[0];
- $argv[2][$i] = $temp[1];
+ $input[$i] = $temp[0];
+ $input[$i] = $temp[1];
 $i++;
         }
     }
 }
 */
-    $temp = explode(",",$argv[2]);
+    $temp = explode(",",$input);
     for($i = 0;$i < count($temp);$i++)
     {
     $sum = $sum + $temp[$i];
     }
     echo $sum;
+    exit;
     }
     }else{
         echo 0;
+        exit;
     }
     }else{
         echo "math operation name does not match";
+        exit;
     }
     }else{
         echo "Please enter the a math operation name";
-        //return;
+        exit;
     }
     echo nl2br("\n");
 }catch(Exception $e){
     echo 'Message: ' .$e->getMessage();
+    exit;
 }
 
 ?>
